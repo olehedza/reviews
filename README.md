f0919302f1e028d187a8a545f9f5be6df701828b
olehedza <olehedza@gmail.com>
2020-07-06 20:37:10 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Spring Boot REST API project that analyze and transform the > 500.000 reviews from amazon
---
+++ src/main/java/com/olehedza/reviews/ReviewsApplication.java
@@ -0,0 +1,11 @@
+package com.olehedza.reviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewsApplication.class, args);
+	}
+}
