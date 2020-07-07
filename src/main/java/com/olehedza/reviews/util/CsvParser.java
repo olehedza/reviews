8e31dc797d23871e29abd3a64a70181f008b1ed5
olehedza <olehedza@gmail.com>
2020-07-08 01:20:11 +0300

    add functionality to read the file

--- src/main/java/com/olehedza/reviews/util/CsvParser.java
+++ src/main/java/com/olehedza/reviews/util/CsvParser.java
@@ -1,57 +1,56 @@
 package com.olehedza.reviews.util;
 
 import com.olehedza.reviews.dto.parser.CsvDto;
-import java.io.StringReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.SneakyThrows;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
+@Getter
 @Slf4j
 public class CsvParser implements Parser<CsvDto> {
-    private final List<CsvDto> dtos = new ArrayList<>();
-    private final FileReader fileReader;
+    private final List<CsvDto> csvDtoList;
 
-    public CsvParser(FileReader fileReader) {
-        this.fileReader = fileReader;
+    public CsvParser() {
+        this.csvDtoList = new ArrayList<>();
     }
 
-    @SneakyThrows
     @Override
     public List<CsvDto> parse(String path) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (String string : fileReader.readFile(path)) {
-            stringBuilder
-                    .append(string)
-                    .append("\n");
-        }
-        Iterable<CSVRecord> records = CSVFormat.DEFAULT
-                .withFirstRecordAsHeader()
-                .parse(new StringReader(stringBuilder.toString()));
-        for (CSVRecord record : records) {
-            if (record.size() == CsvHeaders.values().length) {
-                CsvDto dto = CsvDto.builder()
-                        .productId(record.get(CsvHeaders.ProductId))
-                        .userId(record.get(CsvHeaders.UserId))
-                        .profileName(record.get(CsvHeaders.ProfileName))
-                        .helpfulnessNumerator(Integer.parseInt(record
-                                .get(CsvHeaders.HelpfulnessNumerator)))
-                        .helpfulnessDenominator(Integer.parseInt(record
-                                .get(CsvHeaders.HelpfulnessDenominator)))
-                        .score(Integer.parseInt(record.get(CsvHeaders.Score)))
-                        .time(Long.parseLong(record.get(CsvHeaders.Time)))
-                        .summary(record.get(CsvHeaders.Summary))
-                        .text(record.get(CsvHeaders.Text))
-                        .build();
-                log.info(dto.toString());
-                dtos.add(dto);
+        try (Reader in = new FileReader(path)) {
+            Iterable<CSVRecord> records = CSVFormat.DEFAULT
+                    .withFirstRecordAsHeader().parse(in);
+            for (CSVRecord record : records) {
+                if (record.size() == CsvHeaders.values().length) {
+                    CsvDto dto = CsvDto.builder()
+                            .productId(record.get(CsvHeaders.ProductId))
+                            .userId(record.get(CsvHeaders.UserId))
+                            .profileName(record.get(CsvHeaders.ProfileName))
+                            .helpfulnessNumerator(Integer.parseInt(record
+                                    .get(CsvHeaders.HelpfulnessNumerator)))
+                            .helpfulnessDenominator(Integer.parseInt(record
+                                    .get(CsvHeaders.HelpfulnessDenominator)))
+                            .score(Integer.parseInt(record.get(CsvHeaders.Score)))
+                            .time(Long.parseLong(record.get(CsvHeaders.Time)))
+                            .summary(record.get(CsvHeaders.Summary))
+                            .text(record.get(CsvHeaders.Text))
+                            .build();
+                    csvDtoList.add(dto);
+                }
             }
+        } catch (IOException e) {
+            log.error("File not founded ", e);
         }
-        return dtos;
+        return csvDtoList;
     }
 
     enum CsvHeaders {
