27c1d6612fa5dc9663ba9d1ea875c6af6ac618d6
Oleksii <olehedza@gmail.com>
2020-07-13 16:49:43 +0300

    add functionality to read the file (#2)
    * start simple Spring Boot app
    
    * add functionality to read the file
    
    * add unittest for csv parser
    
    * start simple Spring Boot app (#1)
    
    * add functionality to read the file
    
    * add unittest for csv parser
    
    * change .gitignore list
    
    * add read file functionality
    
    * remove parser functionality
    
    * change file path
    
    * add message to exception constructor
    
    * refactor
    
    * change test signature

---
+++ src/main/java/com/olehedza/reviews/util/CsvFileReader.java
@@ -0,0 +1,33 @@
+package com.olehedza.reviews.util;
+
+import java.io.IOException;
+import java.lang.reflect.UndeclaredThrowableException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import lombok.Cleanup;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CsvFileReader implements FileReader {
+
+    @Override
+    public List<String> readFile(String path) {
+        try {
+            URI resource = Objects.requireNonNull(CsvFileReader.class
+                    .getClassLoader().getResource(path)).toURI();
+            @Cleanup Stream<String> stream = Files.lines(Paths.get(resource));
+            return stream.collect(Collectors.toList());
+        } catch (IOException | URISyntaxException | NullPointerException e) {
+            throw new UndeclaredThrowableException(e, "Can't read file");
+        }
+    }
+}
