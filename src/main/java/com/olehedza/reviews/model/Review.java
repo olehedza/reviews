82fac3d0a4f7024d8865e8e5db2ff78a3a8b8d95
olehedza <olehedza@gmail.com>
2020-07-13 17:38:25 +0300

    change product, review entities

--- src/main/java/com/olehedza/reviews/model/Review.java
+++ src/main/java/com/olehedza/reviews/model/Review.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +16,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "reviews")
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Review {
     @Id
