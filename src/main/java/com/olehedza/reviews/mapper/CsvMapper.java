8bb0807fdffb824c749825239c15116cd7667822
olehedza <olehedza@gmail.com>
2020-07-13 17:39:19 +0300

    add csv mapper

---
+++ src/main/java/com/olehedza/reviews/mapper/CsvMapper.java
@@ -0,0 +1,41 @@
+package com.olehedza.reviews.mapper;
+
+import com.olehedza.reviews.dto.parser.CsvDto;
+import com.olehedza.reviews.model.Product;
+import com.olehedza.reviews.model.Review;
+import com.olehedza.reviews.model.User;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.TimeZone;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class CsvMapper {
+
+    public Review getReviewFromDto(CsvDto dto) {
+        return Review.builder()
+                .score(dto.getScore())
+                .helpfulnessDenominator(dto.getHelpfulnessDenominator())
+                                .helpfulnessNumerator(dto.getHelpfulnessNumerator())
+                .time(LocalDate.ofInstant(Instant.ofEpochSecond(dto.getTime()),
+                        TimeZone.getTimeZone("UTC").toZoneId()))
+                .summary(dto.getSummary())
+                .text(dto.getText())
+                .build();
+    }
+
+    public User getUserFromDto(CsvDto dto) {
+        User user = new User();
+        user.setUserId(dto.getUserId());
+        user.setProfileName(dto.getProfileName());
+        return user;
+    }
+
+    public Product getProductFromDto(CsvDto dto) {
+        Product product = new Product();
+        product.setProductId(dto.getProductId());
+        return product;
+    }
+}
