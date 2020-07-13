11aabe07bc9a7118f41ed38b9cef32728b16e9ba
olehedza <olehedza@gmail.com>
2020-07-13 17:37:09 +0300

    refactor csv reader

--- src/main/java/com/olehedza/reviews/util/CsvFileReader.java
+++ src/main/java/com/olehedza/reviews/util/CsvFileReader.java
@@ -2,9 +2,6 @@ package com.olehedza.reviews.util;
 
 import java.io.IOException;
 import java.lang.reflect.UndeclaredThrowableException;
-<<<<<<< HEAD
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
