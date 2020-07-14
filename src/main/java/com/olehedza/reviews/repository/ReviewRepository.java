e1ebf1b86acfca0ce49f5da25d4108d22b8bf397
olehedza <olehedza@gmail.com>
2020-07-14 13:09:51 +0300

    re-change repository

--- src/main/java/com/olehedza/reviews/repository/ReviewRepository.java
+++ src/main/java/com/olehedza/reviews/repository/ReviewRepository.java
@@ -1,7 +1,10 @@
 package com.olehedza.reviews.repository;
 
 import com.olehedza.reviews.model.Review;
+import com.olehedza.reviews.model.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
