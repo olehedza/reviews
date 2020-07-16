8ead8cbcbf2683d830f5df94a6923bb4fafb8b86
olehedza <olehedza@gmail.com>
2020-07-16 23:57:28 +0300

    refactor service layer

--- src/main/java/com/olehedza/reviews/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/olehedza/reviews/service/impl/ReviewServiceImpl.java
@@ -1,8 +1,11 @@
 package com.olehedza.reviews.service.impl;
 
 import com.olehedza.reviews.model.Review;
+import com.olehedza.reviews.model.Word;
 import com.olehedza.reviews.repository.ReviewRepository;
 import com.olehedza.reviews.service.ReviewService;
+import com.olehedza.reviews.service.WordService;
+import com.olehedza.reviews.util.Splitter;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +13,14 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class ReviewServiceImpl implements ReviewService {
     private final ReviewRepository repository;
+    private final WordService wordService;
+    private final Splitter splitter;
 
     @Override
     public Review addReview(Review review) {
+        for (String word : splitter.split(review.getText())) {
+            wordService.addWord(new Word(word));
+        }
         return repository.save(review);
     }
 }
