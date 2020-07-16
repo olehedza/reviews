8f87b156ac44647c1663ba5689e1220bab27aa6d
olehedza <olehedza@gmail.com>
2020-07-16 23:56:23 +0300

    refactor models

--- src/main/java/com/olehedza/reviews/model/Word.java
+++ src/main/java/com/olehedza/reviews/model/Word.java
@@ -1,6 +1,7 @@
 package com.olehedza.reviews.model;
 
 import javax.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,9 +9,16 @@ import lombok.NoArgsConstructor;
 @Table(name = "words")
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Word {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String word;
+    @Transient
+    private Long count;
+
+    public Word(String word) {
+        this.word = word;
+    }
 }
